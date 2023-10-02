a15f9f9fa0e8b0033badf68342a5bb505e235f8f
TAKA <gaoming93@foxmail.com>
2023-10-02 18:12:23 +0800

    Be able to  string together several operations and get the right answer

--- app.js
+++ app.js
@@ -11,43 +11,50 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
-let number1, number2, operator;
+let number1, number2, operator, accumulator,currentValue;
 function operate (number1, operator, number2) {
     return operator(number1,number2);
 };
 
-
-const buttons = document.querySelectorAll("button");
 let displayValue = document.querySelector(".display");
-
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equal");
+
 numbers.forEach(number => {number.addEventListener("click",(e) => {
-    let numberChoose = parseInt(e.target.innerText);
-    if (operator === undefined) {
-        if (number1 === undefined) {number1 = numberChoose;
-        } else {number1 = number1 * 10 + numberChoose;}    
+    let numberChoose = Number(e.target.innerText);
+    if (operator === undefined) { // user entering first number
+        number1 === undefined ? number1 = numberChoose : number1 = number1 * 10 + numberChoose;  
         displayValue.innerText = number1;
     } else {
-        if (number2 === undefined) {number2 = numberChoose;
-        } else {number2 = number2 * 10 + numberChoose;}        
-        displayValue.innerText = number2;
+        if (!accumulator) {
+            number2 === undefined ? number2 = numberChoose : number2 = number2 * 10 + numberChoose;     
+            displayValue.innerText = number2;
+            accumulator = operate(number1,window[operator],number2);
+        } else {
+            currentValue === undefined ? currentValue = numberChoose : currentValue = currentValue * 10 + numberChoose;
+            displayValue.innerText = currentValue;
+            accumulator = operate(accumulator,window[operator],currentValue);
+        }
     }    
 })    
 });
-
-const operators = document.querySelectorAll(".operator");
-operators.forEach(oper => {oper.addEventListener("click",(e) => operator = e.target.value)    
+    
+operators.forEach(oper => {oper.addEventListener("click",(e) => {
+    operator = e.target.value;
+    currentValue = undefined;
+}
+)    
 });
 
-const equal = document.querySelector("#equal");
 equal.addEventListener("click",()=>{
-    displayValue.innerText = operate(number1,window[operator],number2);
+    displayValue.innerText = accumulator;    
 })
 
 const clear = document.querySelector("#clear");
 clear.addEventListener("click",()=>{
     displayValue.innerText = "";
-    number1 = undefined;
-    number2 = undefined;
+  
+    number1, number2, accumulator,currentValue,operator = undefined;
 })
 
